675d40fc588151c224d13b3c0680705f1861cd44
SamaWyna <etornamasamoah@gmail.com>
2023-11-13 10:32:57 +0000

    New Recurring from Plumm
    Logic working so far with timezone.
    
    Daily Recurrence:
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-01-31",
        "repeat": "Daily",
        "repeat_every": 1,
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }
    
    Weekly Recurrence:
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-01-31",
        "repeat": "Weekly",
        "repeat_every": 1,
        "repeat_on": ["2", "4"],  // Monday and Wednesday
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }
    
    Monthly Recurrence:
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-12-31",
        "repeat": "Monthly",
        "repeat_every": 1,
        "repeat_on_day": 15,
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }
    
    Yearly Recurrence:
    
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-12-31",
        "repeat": "Yearly",
        "repeat_every": 1,
        "repeat_on_month": "December",
        "repeat_on_day": 25,
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }

--- slots/api/views.py
+++ slots/api/views.py
@@ -1,9 +1,11 @@
 from datetime import datetime, date, timedelta
 
+import pytz
 from click.core import F
 from dateutil.relativedelta import relativedelta
 from django.contrib.auth import get_user_model
 from django.db.models import Q, ExpressionWrapper
+from django.http import JsonResponse
 from django.utils import timezone
 from rest_framework import status, fields
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
@@ -12,7 +14,7 @@ from rest_framework.response import Response
 from accounts.models import Company
 from activities.models import AllActivity
 from slots.api.serializers import AppointmentSlotSerializer
-from slots.models import AppointmentSlot, TimeSlot
+from slots.models import AppointmentSlot, TimeSlot, PlummHealthRecurring
 
 User = get_user_model()
 
@@ -1533,13 +1535,164 @@ def set_recurring_list_slot2222(request):
 
 
 
-
 @api_view(['POST', ])
-@permission_classes([])
-@authentication_classes([])
+@permission_classes([])  # Allowing any user to access this endpoint, modify as needed
+@authentication_classes([])  # No authentication for simplicity, add authentication as needed
 def set_recurring_slot_plumm(request):
-    payload = {}
-    data = {}
-    errors = {}
+    REPEAT_ON_CHOICES = [
+        ('Daily', 'Daily'),
+        ('Weekly', 'Weekly'),
+        ('Monthly', 'Monthly'),
+        ('Yearly', 'Yearly')
+    ]
 
-    return Response(payload)
+    END_REPEAT_CHOICES = [
+        ('Never', 'Never'),
+        ('on', 'on'),
+        ('After', 'After'),
+    ]
+
+    if request.method == 'POST':
+        # Get parameters from the POST data
+        start_date_str = request.data.get('start_date')
+        end_date_str = request.data.get('end_date')
+        repeat = request.data.get('repeat')
+        repeat_every = int(request.data.get('repeat_every', 1))
+        repeat_on = request.data.get('repeat_on', [])
+        repeat_on_day = int(request.data.get('repeat_on_day', 1))
+        repeat_on_month = request.data.get('repeat_on_month')
+        end_repeat = request.data.get('end_repeat')
+        end_repeat_on_str = request.data.get('end_repeat_on')
+        end_repeat_after = int(request.data.get('end_repeat_after', 1))
+        recurring_time_str = request.data.get('recurring_time')  # Time in 'HH:MM' format
+        timezone_str = request.data.get('timezone', 'GMT')
+
+        # Get the time zone from the API data
+        timezone = pytz.timezone(timezone_str)
+
+        # Convert date strings to datetime objects with time zone information
+        start_date = timezone.localize(datetime.strptime(start_date_str, '%Y-%m-%d')).date()
+        end_date = timezone.localize(datetime.strptime(end_date_str, '%Y-%m-%d')).date() if end_date_str else None
+        end_repeat_on = timezone.localize(datetime.strptime(end_repeat_on_str, '%Y-%m-%d')).date() if end_repeat_on_str else None
+
+        # Convert recurring_time string to time object
+        recurring_time = datetime.strptime(recurring_time_str, '%H:%M').time()
+
+        # Validate parameters (you may want to add more validation logic)
+        if repeat not in [choice[0] for choice in REPEAT_ON_CHOICES]:
+            return Response({'error': 'Invalid repeat option'}, status=400)
+
+        if end_repeat not in [choice[0] for choice in END_REPEAT_CHOICES]:
+            return Response({'error': 'Invalid end repeat option'}, status=400)
+
+        # Generate recurring slots with the user-provided recurring time
+        recurring_slots = generate_recurring_slots_with_time_and_repeat_on(
+            start_date, end_date, repeat, repeat_every,
+            repeat_on, repeat_on_month, repeat_on_day,
+            end_repeat, end_repeat_on, end_repeat_after,
+            recurring_time=recurring_time
+        )
+
+        # Save recurring slots to the database
+        # save_recurring_slots_to_database(request.user, recurring_slots)
+
+        # Return a JSON response with the recurring dates
+        return Response({'recurring_dates': [date.isoformat() for date in recurring_slots]})
+
+    # If the request method is not POST, return an error response
+    return Response({'error': 'Invalid request method'}, status=400)
+
+
+
+def generate_recurring_slots_with_time_and_repeat_on(start_date, end_date, repeat, repeat_every,
+                                       repeat_on_list, repeat_on_month, repeat_on_day,
+                                       end_repeat, end_repeat_on, end_repeat_after,
+                                       recurring_time):
+    recurring_slots = []
+
+    current_date = start_date
+    while current_date <= end_date:
+        # Check if the current date satisfies the repeat conditions
+        if is_repeating_with_repeat_on(current_date, repeat, repeat_on_list, repeat_on_month, repeat_on_day):
+            # Combine the date and user-provided time to create a datetime object
+            recurring_datetime = datetime.combine(current_date, recurring_time)
+            recurring_slots.append(recurring_datetime)
+
+        # Move to the next date based on the repeat frequency
+        current_date += timedelta(days=repeat_every)
+
+        # Check if we've reached the end repeat conditions
+        if is_end_reached(current_date, end_repeat, end_repeat_on, end_repeat_after):
+            break
+
+    return recurring_slots
+
+MONTH_CHOICES = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December': 12,
+}
+
+def is_repeating_with_repeat_on(current_date, repeat, repeat_on_list, repeat_on_month, repeat_on_day):
+    # Implement logic to check if the current date satisfies the repeat conditions
+    if repeat == 'Daily':
+        return True  # Daily logic (always true)
+
+    elif repeat == 'Weekly':
+        # Check if the current day of the week is in the repeat_on_list
+        return str(current_date.weekday()) in repeat_on_list
+
+    elif repeat == 'Monthly':
+        # Check if the current day of the month matches the specified repeat_on_day
+        return current_date.day == repeat_on_day
+
+    elif repeat == 'Yearly':
+        # Check if the current day, month, and year match the specified repeat_on_day, repeat_on_month, and current year
+        return (
+            current_date.day == repeat_on_day and
+            current_date.month == MONTH_CHOICES[repeat_on_month] and
+            current_date.year == datetime.now().year
+        )
+
+    # Default to False if repeat option is not recognized
+    return False
+def is_end_reached(current_date, end_repeat, end_repeat_on, end_repeat_after):
+    # Implement logic to check if the end repeat conditions are met
+    if end_repeat == 'Never':
+        return False
+
+    elif end_repeat == 'on':
+        return current_date == end_repeat_on
+
+    elif end_repeat == 'After':
+        return str(current_date) > str(end_repeat_after)
+
+    # Default to False if end repeat option is not recognized
+    return False
+
+def save_recurring_slots_to_database(user, recurring_slots):
+    # Save recurring slots to the database
+    for slot_date in recurring_slots:
+        recurring_slot = PlummHealthRecurring(
+            user_id=user,
+            start_date=slot_date,
+            end_date=slot_date,  # Assuming the end_date is the same as start_date for simplicity
+            repeat='Daily',  # You may want to add a 'Custom' option to the choices for clarity
+            repeat_every=1,
+            repeat_on=None,
+            repeat_on_day=None,
+            repeat_on_month=None,
+            end_repeat='Never',  # Assuming 'Never' for simplicity
+            end_repeat_on=None,
+            end_repeat_after=None
+        )
+        recurring_slot.save()
