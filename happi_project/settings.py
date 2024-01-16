e9c0c30426f2a6fb71c9452b9cd2d04887c110a5
samawyna <etornamasamoah@gmail.com>
2024-01-16 04:02:19 +0000

    Dockerised Project - Added docker config files - Added filters for appointment status - (start and end date) - Logic working so far with timezone.
    Daily Recurrence:
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-01-31",
        "repeat": "Daily",
        "repeat_every": 1,
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }
    
    Weekly Recurrence:
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-01-31",
        "repeat": "Weekly",
        "repeat_every": 1,
        "repeat_on": ["2", "4"],  // Monday and Wednesday
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }
    
    Monthly Recurrence:
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-12-31",
        "repeat": "Monthly",
        "repeat_every": 1,
        "repeat_on_day": 15,
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }
    
    Yearly Recurrence:
    
    {
        "pract_id": "98kac8stqk55pbq60crj54237upz3l109uggll",
        "company_id": "hzvun4fbc",
        "timezone": "GMT",
        "start_date": "2023-01-01",
        "end_date": "2023-12-31",
        "repeat": "Yearly",
        "repeat_every": 1,
        "repeat_on_month": "December",
        "repeat_on_day": 25,
        "end_repeat": "After",
        "end_repeat_after": 5,
        "recurring_time": "19:00"
    }

--- happi_project/settings.py
+++ happi_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-tkvixfgpyk2$1!p^0m=*01hl_0cf6%-hzzs9wc(e5(l_^so&jx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.43.220", "127.0.0.1", "0.0.0.0", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost", "143.42.21.193"]
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
